a3449b3908d8ab5f66a1d0e244d58d7ec8956a57
Charwik <94569341+Charwik@users.noreply.github.com>
2022-03-12 01:09:56 -0600

    Add files via upload

---
+++ Cuestionario/lilia.js
@@ -0,0 +1,107 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "El grupo Liliaceae-Amaryllidaceae, está representado por plantas bulbosas perennes.",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Sus flores presentan un perianto diferenciado en cáliz y corola.",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Sus flores son estrictamente hipóginas.",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Presentan tallos modificados en bulbos, cormos y rizomas.",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Su distribución es cosmopolita.",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Sus flores son típicamente inconspicuas.",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  {
+    numb: 7,
+    question: "En ocasiones presenta especies con hojas de más de 1 metro de consistencia carnosa",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  {
+    numb: 8,
+    question: "El mezcal de Oaxaca se obtiene de la especie Agave lechuguilla.",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Colchicum autumnale es fuente de colchicina, utilizada en genética vegetal.",
+    answer: "Verdadero",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  {
+    numb: 10,
+    question: "De Asparagus setaceus (espárrago), se consumen sus brotes tiernos.",
+    answer: "Falso",
+    options: [
+      "Verdadero",
+      "Falso"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
