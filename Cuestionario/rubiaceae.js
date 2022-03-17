3bd05a5a65e9f399d0b984f32e961f6a99824d88
Charwik <94569341+Charwik@users.noreply.github.com>
2022-03-17 14:27:25 -0600

    Add files via upload

---
+++ Cuestionario/rubiaceae.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Las formas de vida presentes en la familia Rubiaceae son:",
+    answer: "Árboles, arbustos y hierbas",
+    options: [
+      "Herbáceas anuales",
+      "Hierbas y arbustos",
+      "Árboles, arbustos y hierbas"
+      
+    ]
+  },
+    {
+    numb: 2,
+    question: "Por su filotaxia se caracterizan como:",
+    answer: "Hojas opuestas",
+    options: [
+      "Hojas verticiladas",
+      "Hojas opuestas",
+      "Hojas alternas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "En cuanto a las estípulas pueden estar:",
+    answer: "Interpeciolares",
+    options: [
+      "Interpeciolares",
+      "Ausentes",
+      "En la base del pecíolo"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Las inflorescencias frecuentes son:",
+    answer: "Cimas y Dicasios",
+    options: [
+      "Racimos y umbelas",
+      "Espigas y amentos",
+      "Cimas y Dicasios"
+    ]
+  },
+    {
+    numb: 5,
+    question: "En relación con la parte terminal del pistilo son características de ser:",
+    answer: "Heterostilas",
+    options: [
+      "Heterostilas",
+      "Inclusas",
+      "Extrorsas"
+    ]
+  },
+  {
+      numb: 6,
+  question: "Las flores por la posición del ovario son:",
+  answer:  "Epigíneas",
+  options: [
+    "Hipóginas",
+      "Epigíneas",
+      "Perigíneas"
+  ]
+},
+{
+    numb: 7,
+question: "Cultivo de importancia mundial:",
+answer: "Coffea arabica",
+options: [
+   "Chinchona officinalis",
+      "Morinda citrifolia",
+      "Coffea arabica"
+]
+},
+{
+    numb: 8,
+question: "Especie medicinal utilizada para la malaria:",
+answer: "Chinchona officinalis",
+options: [
+  "Chinchona officinalis",
+  "Morinda citrifolia",
+  "Coffea arabica"
+]
+},
+{
+    numb: 9,
+question: "Especie fuente de estimulación sexual:",
+answer: "Pausinystalia johimbe",
+options: [
+  "Uragoga ipecacuana",
+      "Pausinystalia johimbe",
+      "Hamelia patens"
+]
+},
+{
+    numb: 10,
+question: "El jugo denominado” Noni”, está elaborado a base de:",
+answer: "Morinda citrifolia",
+options: [
+  "Morinda citrifolia",
+  "Randia maculata",
+  "Bouvardia ternifolia"
+]
+},
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
